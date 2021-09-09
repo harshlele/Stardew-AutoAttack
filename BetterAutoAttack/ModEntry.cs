47f4b65685d1e150c2d347cb558a70fb7cbe0e56
Harshal Lele <harshallele97@gmail.com>
2021-09-09 23:16:44 +0530

    added basic attack system

--- BetterAutoAttack/ModEntry.cs
+++ BetterAutoAttack/ModEntry.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
+using StardewValley.Monsters;
+using StardewValley.Tools;
+
 
 namespace BetterAutoAttack
 {
     /// <summary>The mod entry point.</summary>
     public class ModEntry : Mod
     {
-        private IReflectionHelper Reflection;
+        private IReflectionHelper reflection;
+
         /*********
         ** Public methods
         *********/
@@ -18,73 +23,67 @@ namespace BetterAutoAttack
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+            helper.Events.Input.ButtonPressed += Util.ButtonPressTest;
             helper.Events.GameLoop.UpdateTicked += this.onUpdateTicked;
-            Reflection = this.Helper.Reflection;
+            reflection = this.Helper.Reflection;
         }
 
 
-        /*********
-        ** Private methods
-        *********/
-        /// <summary>
-        /// Used for testing attack algorithm
-        /// </summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event data.</param>
-        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
-        {
-            // ignore if player hasn't loaded a save yet
-            if (!Context.IsWorldReady)
-                return;
-
-            //do attack action
-            if(e.Button == SButton.Z)
-            {
-                log(Game1.player.FacingDirection.ToString());
-                Game1.player.BeginUsingTool();
-                
-            }
-            //do secondary attack function
-            if (e.Button == SButton.P)
-            { 
-                ((StardewValley.Tools.MeleeWeapon)Game1.player.CurrentTool).animateSpecialMove(Game1.player);
-            }
-
-            //spawn a ground monster
-            if (e.Button == SButton.V)
-            {
-                //Game1.getFarm().spawnGroundMonsterOffScreen();
-
-                Rectangle r = Utility.getRectangleCenteredAt(Game1.player.getTileLocation(), 10);
-                Vector2 spawnAt = Utility.getRandomPositionInThisRectangle(r, new Random());
-                Game1.getFarm().characters.Add(new StardewValley.Monsters.GreenSlime(spawnAt * 64f, 41)
-                {
-                    focusedOnFarmers = true,
-                    wildernessFarmMonster = true
-                });
-            }
-            //spawn a flying bat
-            if(e.Button == SButton.O)
-            {
-                Rectangle r = Utility.getRectangleCenteredAt(Game1.player.getTileLocation(), 10);
-                Vector2 spawnAt = Utility.getRandomPositionInThisRectangle(r, new Random());
-                Game1.getFarm().characters.Add(new StardewValley.Monsters.Bat(spawnAt * 64f, 41)
-                {
-                    focusedOnFarmers = true,
-                    wildernessFarmMonster = true
-                });
-            }
-        }
 
         private void onUpdateTicked(object sender, UpdateTickedEventArgs args)
         {
-
+            if (!Context.IsWorldReady)
+            {
+                return;
+            }
+            Farmer player = Game1.player;
+            GameLocation location = Game1.player.currentLocation;
+            List<NPC> monsters = getMonstersWithinDistance(player.getTileLocation(), 2, location);
+            if (!player.isActive() || player.CurrentTool == null || !(player.CurrentTool is MeleeWeapon) || monsters.Count <= 0)
+            {
+                return;
+            }
+            Monster toAttack = (Monster) monsters[0];
+            bool blockAttack = false;
+            foreach (Monster m in monsters)
+            {
+                if(m.health > toAttack.health)
+                {
+                    toAttack = m;
+                }
+                if(Vector2.Distance(m.getTileLocation(), player.getTileLocation()) <= 1){
+                    toAttack = m;
+                    blockAttack = true;
+                    break;
+                }
+            }
+            int direction = player.getGeneralDirectionTowards(toAttack.Position);
+            if (player.FacingDirection != direction)
+            {
+                player.faceDirection(direction);   
+            }
+            if (!player.IsBusyDoingSomething())
+            {
+                if (!blockAttack) player.BeginUsingTool();
+                else ((MeleeWeapon)player.CurrentTool).animateSpecialMove(player);
+            }
+            
         }
 
-        private void log(string s)
+        private static List<NPC> getMonstersWithinDistance(Vector2 tileLocation, int tilesAway, GameLocation environment)
         {
-            this.Monitor.Log(s, LogLevel.Debug);
+            List<NPC> chars = new List<NPC>();
+            foreach (NPC c in environment.characters)
+            {
+                if (Vector2.Distance(c.getTileLocation(), tileLocation) <= (float)tilesAway && c.IsMonster && !c.GetType().Equals(Game1.player))
+                {
+                    chars.Add(c);
+                }
+            }
+            return chars;
         }
+
+
+
     }
 }
---
+++ BetterAutoAttack/Util.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using Microsoft.Xna.Framework;
+
+namespace BetterAutoAttack
+{
+    static class Util
+    {
+        /// <summary>
+        /// Used for generating monsters, doing attack/defense actions etc
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        public static void ButtonPressTest(object sender, ButtonPressedEventArgs e)
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+
+            //do attack action
+            if (e.Button == SButton.Z)
+            {
+                Game1.player.BeginUsingTool();
+
+            }
+            //do secondary attack function
+            if (e.Button == SButton.P)
+            {
+                ((StardewValley.Tools.MeleeWeapon)Game1.player.CurrentTool).animateSpecialMove(Game1.player);
+            }
+
+            //spawn a ground monster
+            if (e.Button == SButton.V)
+            {
+                //Game1.getFarm().spawnGroundMonsterOffScreen();
+
+                Rectangle r = Utility.getRectangleCenteredAt(Game1.player.getTileLocation(), 10);
+                Vector2 spawnAt = Utility.getRandomPositionInThisRectangle(r, new Random());
+                Game1.getFarm().characters.Add(new StardewValley.Monsters.GreenSlime(spawnAt * 64f, 41)
+                {
+                    focusedOnFarmers = true,
+                    wildernessFarmMonster = true
+                });
+            }
+            //spawn a flying bat
+            if (e.Button == SButton.O)
+            {
+                Rectangle r = Utility.getRectangleCenteredAt(Game1.player.getTileLocation(), 10);
+                Vector2 spawnAt = Utility.getRandomPositionInThisRectangle(r, new Random());
+                Game1.getFarm().characters.Add(new StardewValley.Monsters.Bat(spawnAt * 64f, 41)
+                {
+                    focusedOnFarmers = true,
+                    wildernessFarmMonster = true
+                });
+            }
+
+            if (SButtonExtensions.IsUseToolButton(e.Button)){
+                Game1.player.completelyStopAnimatingOrDoingAction();
+            }
+            
+
+        }
+
+        public static void Log(ModEntry e,string s)
+        {
+            e.Monitor.Log(s, LogLevel.Debug);
+        }
+
+    }
+}
